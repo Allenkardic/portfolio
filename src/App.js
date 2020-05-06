c65ea924380a76d9ea700c0ff179048161f0e661
aliakwe <aliakwe@yahoo.com>
2020-05-06 04:02:32 +0100

    implement error page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
-import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Projects from "./components/Projects";
 import Notfound from "./components/Notfound";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Navbar /> */}
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/projects" component={Projects} />
           <Route component={Notfound} />
         </Switch>
       </Router>
--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -1,11 +1,17 @@
 /** @format */
 
 import React from "react";
+import { Link } from "react-router-dom";
+import "../App.scss";
 
 function Notfound() {
   return (
-    <div>
-      <div>not found</div>
+    <div className="error-page">
+      <div>404</div>
+      <div>SORRY, PAGE NOT FOUND</div>
+      <Link to="/">
+        <div className="btn-error">Home</div>
+      </Link>
     </div>
   );
 }
